dc12ce70d386cc1d156b7546f18c9d8e7e713bca
AlDacMac <alasdairmacgsign@gmail.com>
2021-03-18 18:07:26 +0000

    rename slice lengthin the slice_time function

--- slouch_buffer.py
+++ slouch_buffer.py
@@ -6,7 +6,7 @@ SLICE_LENGTH = 60 #This is in seconds
 #Gives the current time slice in terms of the seconds_since_epoch when it began
 def sliced_time():
     cur_time = int(time.time())
-    time_into_slice = cur_time % slice_length
+    time_into_slice = cur_time % SLICE_LENGTH
     cur_time -= time_into_slice
     return cur_time
 
