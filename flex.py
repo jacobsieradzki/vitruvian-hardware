3f7a9060936bb9eb41118d16406c06deffd8763a
WormyUltra <andrew.co.robertson@gmail.com>
2021-03-22 14:50:27 +0000

    flex sensor from friday testing

--- flex.py
+++ flex.py
@@ -7,6 +7,7 @@ import time
 from grove.adc import ADC
 from multiprocessing import Process
 import cv2
+import os
 
 #has a specific channel and can read adc value from it
 class GroveFlex(ADC):
@@ -66,14 +67,35 @@ def main():
     #data reading
     sensor1 = GroveFlex(0)
     sensor2 = GroveFlex(2)
+
+    ctime = 0
+    n1s = []
+    n2s = []
+    while(ctime < 6):
+	n1s.append(sensor1.value)
+	n2s.append(sensor2.value)
+	ctime += 0.25
+	time.sleep(0.25)    
+    norm1 = sum(n1s)/len(n1s)
+    norm2 = sum(n2s)/len(n2s)
+    print "calibration complete, n1: {}, n2: {}".format(norm1,norm2)
+
     timer = 0
     with open("test_flex.txt", "w") as f:
         while True:
+	    fl1 = sensor1.value
+	    fl2 = sensor2.value
 	    if (timer % 1 == 0):
-                print('time: {} Flex sensor 1 VD value: {}mV'.format(str(timer), str(sensor1.value)))
-                print('time: {} Flex sensor 2 VD value: {}mV'.format(str(timer), str(sensor2.value)))
-            f.write('time: {} FS 1 VD: {}mV\n'.format(str(timer), str(sensor1.value)))
-	    f.write('time: {} FS 2 VD: {}mV\n'.format(str(timer), str(sensor2.value)))
+                print('time: {} Flex sensor 1 VD value: {}mV'.format(str(timer), str(fl1)))
+                print('time: {} Flex sensor 2 VD value: {}mV'.format(str(timer), str(fl2)))
+            f.write('time: {} FS 1 VD: {}mV\n'.format(str(timer), str(fl1)))
+	    f.write('time: {} FS 2 VD: {}mV\n'.format(str(timer), str(fl2)))
+	    if (abs(fl1 - norm1) > 100) :
+		os.system("python3 anglebuzz.py")
+		print "flex sensor 1 detected"
+	    if (abs(fl2 - norm2) > 100) :
+		os.system("python3 bendpulse.py")
+		print "flex sensor 2 detected"
 	    timer += 0.25
 	    time.sleep(.25)
 
