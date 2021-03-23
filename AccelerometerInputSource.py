d488a5f7579317b056f907b308f18fe79eba3cc1
Jake Ryan <jake@springnation.co.uk>
2021-03-23 14:33:37 +0000

    Fixed imports for mpu/smbus libraries

--- AccelerometerInputSource.py
+++ AccelerometerInputSource.py
@@ -1,21 +1,21 @@
-import smbus
-import FaBo9Axis_MPU9250 as MPU
 from Reading import Reading
-
-bus = smbus.SMBus(1)
-address = 0x70
-mpu = MPU.MPU9250()
 
 
 class MPUInputSource:
-    def __init__(self, path, received_new_reading, recieved_new_norm_reading):
+    def __init__(self, path, received_new_reading):
+        import smbus
+        import FaBo9Axis_MPU9250 as MPU
+
+        self.bus = smbus.SMBus(1)
+        self.address = 0x70
+        self.mpu = MPU.MPU9250()
         self.path = path
         self.received_new_reading = received_new_reading
-        bus.write_byte(address, self.path)
+        self.bus.write_byte(self.address, self.path)
 
     def fetch_new_reading(self, timestamp, calibrate=False):
-        bus.write_byte(address, self.path)
-        acceleration = mpu.readAccel()
+        self.bus.write_byte(self.address, self.path)
+        acceleration = self.mpu.readAccel()
         x = acceleration['y'] + 0.00000001
         y = acceleration['y'] + 0.00000001
         z = acceleration['z'] + 0.00000001
