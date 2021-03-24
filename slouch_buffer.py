13841e6da31d142e05194557135c9a23c675b831
AlDacMac <alasdairmacgsign@gmail.com>
2021-03-24 11:02:58 +0000

    make slouch buffer a class

--- slouch_buffer.py
+++ slouch_buffer.py
@@ -1,28 +1,29 @@
 import time
 
 #Set slice length variable, and initialise current time slice
-SLICE_LENGTH = 60 #This is in seconds
+SLICE_LENGTH = 20 #This is in seconds
 
 #Gives the current time slice in terms of the seconds_since_epoch when it began
-def sliced_time():
+def sliced_time(slice_length):
     cur_time = int(time.time())
-    time_into_slice = cur_time % SLICE_LENGTH
+    time_into_slice = cur_time % slice_length
     cur_time -= time_into_slice
     return cur_time
 
-cur_slice = sliced_time()
-time_slouching = 0
+class slouch_buffer:
+    def __init__(self, slice_length_ms):
+        self.slice_length = slice_length_ms / 1000
+        self.time_slouching = 0.0
+        self.cur_slice = sliced_time(self.slice_length)
 
-def update_buffer(slouching):
-    global cur_slice
-    global time_slouching
-    new_slice = sliced_time()
-    if(new_slice != cur_slice):
-        cur_slice = new_slice
-        score = (SLICE_LENGTH - time_slouching) / float(SLICE_LENGTH) * 100
-        time_slouching = 0
-        return score
-    if slouching:
-        print(str(time_slouching))
-        time_slouching = time_slouching + 4
+    def update_buffer(self, slouching):
+        new_slice = sliced_time(self.slice_length)
+        if(new_slice != self.cur_slice):
+            self.cur_slice = new_slice
+            score = (1  - (self.time_slouching / self.slice_length)) * 100
+            self.time_slouching = 0.0
+            return score
+        if slouching:
+            print(str(self.time_slouching))
+            self.time_slouching = self.time_slouching + 4
  
